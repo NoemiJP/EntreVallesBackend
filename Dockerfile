1ca5c2446465251ecd28d56f450582b65dbc0c51
noemi <mxc58627@educastur.es>
2024-05-19 20:02:12 +0200

    Cross Origin Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -29,4 +29,4 @@ COPY --from=build /app/target/*.jar app.jar
 EXPOSE 8080
 
 # Comando para ejecutar la aplicación
-ENTRYPOINT ["java", "-jar", "app.jar"]
+ENTRYPOINT ["java", "-jar", "-Dspring.profiles.active=deploy", "app.jar"]
---
+++ src/main/java/com/ibq/entrevalles/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.ibq.entrevalles.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*/**")
+                .allowedOrigins("https://entrevalles.onrender.com","http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
